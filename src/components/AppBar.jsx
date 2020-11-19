aa384e9596ff0c34ae8d6022ba7526b1bcd1104d
Layth Barzangi <thebarzangi@gmail.com>
2020-11-20 00:30:56 +0400

    Solved: Exercise 10.7 (Rate Repo App) Made app bar horizontally-scrollable to cater for additional tabs that might extend beyond the screen width

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 
@@ -9,23 +9,27 @@ import AppBarTab from './AppBarTab';
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'row',
     paddingTop: Constants.statusBarHeight + 10,
     backgroundColor: theme.backgroundColors.appBar,
     paddingBottom: 10
+  },
+  tabsContainer: {
+    display: 'flex',
+    flexDirection: 'row'
   }
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to='/'>
-        <AppBarTab>Repositories</AppBarTab>
-      </Link>
-      <Link to='/sign-in'>
-        <AppBarTab>Sign in</AppBarTab>
-      </Link>
+      <ScrollView horizontal style={styles.tabsContainer}>
+        <Link to='/'>
+          <AppBarTab>Repositories</AppBarTab>
+        </Link>
+        <Link to='/sign-in'>
+          <AppBarTab>Sign in</AppBarTab>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
