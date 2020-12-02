c46839b7522343638e2804f3106b4baa77cc21c7
Layth Barzangi <thebarzangi@gmail.com>
2020-12-02 19:07:53 +0400

    Solved: Exercise 10.15 (Rate Repo App) Implemented access to 'AuthStorage' using the 'useContext' hook to store the user's access token after being successfully authorised. Also reset the Apollo Client's store to clear the cache and re-execute all active queries

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,8 +1,14 @@
 import { useMutation } from '@apollo/react-hooks';
+import { useContext } from 'react';
+import { useApolloClient } from '@apollo/client';
 
 import { AUTHORIZE } from '../graphql/mutations';
 
+import AuthStorageContext from '../contexts/AuthStorageContext';
+
 const useSignIn = () => {
+  const apolloClient = useApolloClient();
+  const authStorage = useContext(AuthStorageContext);
   const [ mutate, result ] = useMutation(AUTHORIZE, {
     onError: (error) => {
       console.log(error.graphQLErrors[0].message);
@@ -10,8 +16,10 @@ const useSignIn = () => {
   });
 
   const signIn = async ({ username, password }) => {
-    const response = await mutate({ variables: { credentials: { username, password } } });
-    return response;
+    const { data } = await mutate({ variables: { credentials: { username, password } } });
+    await authStorage.setAccessToken(data.authorize.accessToken);
+    apolloClient.resetStore();
+    return data;
   };
 
   return [signIn, result];
