75bddc9f96bb6d13563a33d6f18333c50df3ffa6
Layth Barzangi <thebarzangi@gmail.com>
2020-12-03 20:37:03 +0400

    Solved: Exercise 10.16 (Rate Repo App) Simplified by dropping use of the Touchable component Rationale: AppBarTab components are always wrapped with React Router's <Link> component, which provides better touch response and visual feedback

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import { TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import Text from './Text';
 
 const styles = StyleSheet.create({
   appBarTab: {
-    margin: 10,
+    padding: 10,
     color: 'white'
   }
 });
 
 const AppBarTab = props => {
-  return (
-    <TouchableWithoutFeedback>
-      <Text
-        style={styles.appBarTab}
-        fontWeight='bold'
-        fontSize='subheading'
-      >
-        {props.children}
-      </Text>
-    </TouchableWithoutFeedback>
+  return ( 
+    <Text
+      style={styles.appBarTab}
+      fontWeight='bold'
+      fontSize='subheading'
+    >
+      {props.children}
+    </Text>
   );
 };
 
