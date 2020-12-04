cfafc8b0b0d8fe85d50d2542b675b3e8d911c9f2
Layth Barzangi <thebarzangi@gmail.com>
2020-12-04 16:45:54 +0400

    Solved: Exercise 10.17 (Rate Repo App) Refactored rendering the repositories list into its own exported component 'RepositoryListContainer' to separate it from the side effect (useRepositories hook) and facilitate testing

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -13,9 +13,7 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({ repositories }) => {
   // get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -33,6 +31,12 @@ const RepositoryList = () => {
       )}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
