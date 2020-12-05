e0266b58535bcf39f149b78421e6703d03993522
Layth Barzangi <thebarzangi@gmail.com>
2020-12-05 19:38:40 +0400

    Solved: Exercise 10.19 (Rate Repo App) Added the query definition GET_REPOSITORY to fetch single repository info from GraphQL backend

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -20,6 +20,22 @@ export const GET_REPOSITORIES = gql`
   }
 `;
 
+export const GET_REPOSITORY = gql`
+  query getRepository($id: ID!) {
+    repository(id: $id) {
+      ownerAvatarUrl
+      fullName
+      description
+      language
+      stargazersCount
+      forksCount
+      reviewCount
+      ratingAverage
+      url
+    }
+  }
+`;
+
 export const AUTHORIZED_USER = gql`
   query {
     authorizedUser {
