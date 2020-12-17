6be9484c55234ce3ec380790f8393655112cc109
Layth Barzangi <thebarzangi@gmail.com>
2020-12-17 23:11:20 +0400

    Solved: Exercise 10.23 (Rate Repo App) Updated to include sorting arguments `orderBy` and `orderDirection` sent by the `RepositoryList` component to facilitate different sorting options

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,10 +3,11 @@ import { useQuery } from '@apollo/react-hooks';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (orderBy, orderDirection) => {
   const [repositories, setRepositories] = useState();
 
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    variables: { orderBy, orderDirection },
     fetchPolicy: 'cache-and-network',
     onError: () => console.log(error)
   });
