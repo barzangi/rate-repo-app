cb0b6c96636f509f2cbd0ff718854296ae303c0c
Layth Barzangi <thebarzangi@gmail.com>
2020-12-17 18:51:19 +0400

    Solved: Exercise 10.22 (Rate Repo App) Implemented a 'Sign up' view that includes a form to add a new user using the `useCreateUser` hook. The hook returns the new user's username which is used to sign them in via the `useSignIn` hook and then redirect the user to the repositories list view

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import * as yup from 'yup';
+import { View } from 'react-native';
+import { Formik } from 'formik';
+import { useHistory } from 'react-router-native';
+
+import { formStyles as styles } from '../styles';
+
+import FormikTextInput from './FormikTextInput';
+import Button from './Button';
+
+import useCreateUser from '../hooks/useCreateUser';
+import useSignIn from '../hooks/useSignIn';
+
+const initialValues = {
+  username: '',
+  password: '',
+  passwordConfirm: ''
+};
+
+const SignUpForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.container}>
+      <FormikTextInput
+        name='username'
+        placeholder='Username'
+        style={styles.textInput}
+        autoCapitalize='none'
+      />
+      <FormikTextInput
+        name='password'
+        placeholder='Password'
+        secureTextEntry={true}
+        style={styles.textInput}
+        autoCapitalize='none'
+      />
+      <FormikTextInput
+        name='passwordConfirm'
+        placeholder='Password confirmation'
+        secureTextEntry={true}
+        style={styles.textInput}
+        autoCapitalize='none'
+      />
+      <Button onPress={onSubmit}>Sign up</Button>
+    </View>
+  );
+};
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, 'Username must be at least 3 characters long')
+    .max(30, 'Username cannot be longer than 30 characters')
+    .required('Username is required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 character long')
+    .max(50, 'Password cannot be longer than 50 characters')
+    .required('Password is required'),
+  passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords do not match')
+    .required('Password confirmation is required')
+});
+
+const SignUp = () => {
+  const [createUser] = useCreateUser();
+  const [signIn] = useSignIn();
+  const history = useHistory();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const userData = await createUser({ username, password });
+      console.log('create user:', userData);
+      const signInData = await signIn({ username: userData.createUser.username, password });
+      console.log('sign in user:', signInData);
+      history.push('/');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <SignUpForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+};
+
+export default SignUp;
