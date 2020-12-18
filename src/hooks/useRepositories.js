b7624cf22b347f6761e13f33d135c39d34be979f
Layth Barzangi <thebarzangi@gmail.com>
2020-12-18 19:17:47 +0400

    Solved: Exercise 10.25 (Rate Repo App) Major rewrite of the `useReposotories` hook code to facilitate infinite scrolling in the repositories list view

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,26 +1,48 @@
-import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = variables => {
-  const [repositories, setRepositories] = useState();
-
-  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+  const { data, error, loading, fetchMore, ...result } = useQuery(GET_REPOSITORIES, {
     variables,
     fetchPolicy: 'cache-and-network',
     onError: () => console.log(error)
   });
 
-  const fetchRepositories = () => {
-    setRepositories(loading ? null : data.repositories);
+  const handleFetchMore = () => {
+    const canFetchMore =
+      !loading && data && data.repositories.pageInfo.hasNextPage;
+  
+    if (!canFetchMore) return;
+
+    fetchMore({
+      query: GET_REPOSITORIES,
+      variables: {
+        after: data.repositories.pageInfo.endCursor,
+        ...variables
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const nextResult = {
+          repositories: {
+            ...fetchMoreResult.repositories,
+            edges: [
+              ...previousResult.repositories.edges,
+              ...fetchMoreResult.repositories.edges
+            ]
+          }
+        };
+
+        return nextResult;
+      }
+    });
   };
 
-  useEffect(() => {
-    fetchRepositories();
-  }, [fetchRepositories]);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return {
+    repositories: data ? data.repositories : undefined,
+    fetchMore: handleFetchMore,
+    loading,
+    ...result
+  };
 };
 
 export default useRepositories;
