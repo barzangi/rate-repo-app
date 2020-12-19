e9963344e03828e2182626b41d4ba4366ea5b7cc
Layth Barzangi <thebarzangi@gmail.com>
2020-12-19 12:09:59 +0400

    Solved: Exercise 10.25 (Rate Repo App) Major rewrite of the `useRepository` hook code to facilitate infinite scrolling of the reviews in the single repository view

--- src/hooks/useRepository.js
+++ src/hooks/useRepository.js
@@ -1,26 +1,51 @@
-import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_REPOSITORY } from '../graphql/queries';
 
-const useRepository = id => {
-  const [repository, setRepository] = useState();
-
-  const { data, error, loading } = useQuery(GET_REPOSITORY, {
-    variables: { id: id },
+const useRepository = variables => {
+  const { data, error, loading, fetchMore, ...result } = useQuery(GET_REPOSITORY, {
+    variables,
     fetchPolicy: 'cache-and-network',
     onError: () => console.log(error)
   });
 
-  const fetchRepository = () => {
-    setRepository(loading ? null : data.repository);
+  const handleFetchMore = () => {
+    const canFetchMore =
+      !loading && data && data.repository.reviews.pageInfo.hasNextPage;
+
+    if (!canFetchMore) return;
+
+    fetchMore({
+      query: GET_REPOSITORY,
+      variables: {
+        after: data.repository.reviews.pageInfo.endCursor,
+        ...variables
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const nextResult = {
+          repository: {
+            ...data.repository,
+            reviews: {
+              ...fetchMoreResult.repository.reviews,
+              edges: [
+                ...previousResult.repository.reviews.edges,
+                ...fetchMoreResult.repository.reviews.edges
+              ]
+            }
+          }
+        };
+
+        return nextResult;
+      }
+    });
   };
 
-  useEffect(() => {
-    fetchRepository();
-  }, [fetchRepository]);
-
-  return { repository, loading, refetch: fetchRepository};
+  return {
+    repository: data ? data.repository : undefined,
+    fetchMore: handleFetchMore,
+    loading,
+    ...result
+  };
 };
 
 export default useRepository;
