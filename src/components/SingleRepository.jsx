781ed305324344ef1d5a31c0e79aa52827035d83
Layth Barzangi <thebarzangi@gmail.com>
2020-12-20 00:51:59 +0400

    Solved: Exercise 10.27 (Rate Repo App) Updated to use the refactored `ReviewItem` component

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,82 +1,17 @@
 import React from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import { useParams } from 'react-router-native';
-import { format, parseISO } from 'date-fns';
-
-import theme from '../theme';
-import Text from './Text';
 
 import RepositoryItem from './RepositoryItem';
+import ReviewItem from './ReviewItem';
 
 import useRepository from '../hooks/useRepository';
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: theme.backgroundColors.mainContainer,
-    padding: 15
-  },
-  rating: {
-    display: 'flex',
-    flex: 0,
-    width: 50,
-    height: 50,
-    borderWidth: 1,
-    borderRadius: 25,
-    borderColor: theme.colors.primary,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  ratingText: {
-    color: theme.colors.primary
-  },
-  details: {
-    flexGrow: 1,
-    flex: 1,
-    marginLeft: 15
-  },
   separator: {
     height: 10
-  },
-  reviewList: {
-    marginTop: 10
   }
 });
-
-const ReviewItem = ({ review }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.rating}>
-        <Text
-          style={styles.ratingText}
-          fontSize='subheading'
-        >
-          {review.rating}
-        </Text>
-      </View>
-      <View style={styles.details}>
-        <Text
-          fontSize='subheading'
-          fontWeight='bold'
-          color='textPrimary'
-        >
-          {review.user.username}
-        </Text>
-        <Text
-          color='textSecondary'
-        >
-          {format(parseISO(review.createdAt), 'dd.MM.yyyy')}
-        </Text>
-        <Text
-          color='textPrimary'
-        >
-          {review.text}
-        </Text>
-      </View>
-    </View>
-  );
-};
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
@@ -101,13 +36,12 @@ const SingleRepository = () => {
             data={reviews}
             onEndReached={onEndReach}
             onEndReachedThreshold={0.5}
-            renderItem={({ item }) => <ReviewItem review={item} />}
+            renderItem={({ item }) => <ReviewItem review={item} isUserReview={false} />}
             keyExtractor={({ id }) => id}
             ListHeaderComponent={() => <RepositoryItem item={repository} singleRepo='true' />}
             ItemSeparatorComponent={ItemSeparator}
           />
         </>
-        
       }
     </>
   );
